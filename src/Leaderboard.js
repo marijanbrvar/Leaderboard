1ea1e065d9b5cb509a94c62bf81df8e001ba9664
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-04 15:19:07 +0200

    Add api module

---
+++ src/Leaderboard.js
@@ -0,0 +1,8 @@
+class Leaderboard {
+  constructor() {
+    this.key = '';
+    this.baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+  }
+}
+
+export default Leaderboard;
